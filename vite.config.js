1a1380dc356d1aa41c2e79e72d2aa1235982e36c
hritikroshansingh <hritiksingh289@gmail.com>
2026-05-15 23:28:47 +0530

    Fix project restructure after moving frontend to root

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/HRBOOKS/",
   plugins: [react(),
     tailwindcss(),
   ],
-
-  base: '/HRBOOKS/',
 })
